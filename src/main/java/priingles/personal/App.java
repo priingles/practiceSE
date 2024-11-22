84f7686ac9b85cc482dc72c63d85783c1a6a4a1b
priingles <kleoac1994@gmail.com>
2024-11-22 21:53:05 +0000

    set up continued(dockerfile updated)

---
+++ src/main/java/priingles/personal/App.java
@@ -0,0 +1,55 @@
+package priingles.personal;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class App {
+
+    private Connection c = null;
+    public void connect() {
+
+        System.out.println("just BETTER!");
+
+        try {
+            //load db driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        int retry = 5;
+
+        for (int i = 0; i < retry; i++) {
+            try {
+                Thread.sleep(5000);
+                c = DriverManager.getConnection("jdbc:mysql://practice-db:3306/employees?useSSL=false", "root", "example");
+                System.out.println("Connected!");
+                Thread.sleep(5000);
+                break;
+            }
+            catch (SQLException sqle) {
+                System.out.println("Failed!");
+                System.out.println(sqle.getMessage());
+            }
+            catch (InterruptedException ie) {
+                System.out.println("Failed!, Interrupted");
+            }
+        }
+
+    }
+
+    public void disconnect() {
+        if (c != null) {
+            try {
+                c.close();
+                System.out.println("Disconnected!");
+            }
+            catch (Exception e) {
+                System.out.println("Failed to close!");
+            }
+        }
+    }
+}
--- src/main/java/priingles/personal/Main.java
+++ src/main/java/priingles/personal/Main.java
@@ -1,48 +1,11 @@
 package priingles.personal;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
-
 public class Main {
     public static void main(String[] args) {
+        App a = new App();
 
-        System.out.println("just BETTER!");
+        a.connect();
 
-        try {
-            //load db driver
-            Class.forName("com.mysql.cj.jdbc.Driver");
-
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-
-        Connection c = null;
-        int retry = 5;
-
-        for (int i = 0; i < retry; i++) {
-            try {
-                Thread.sleep(1000);
-                c = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false", "root", "example");
-                System.out.println("Connected!");
-                break;
-            }
-            catch (SQLException sqle) {
-                System.out.println("Failed!");
-                System.out.println(sqle.getMessage());
-            }
-            catch (InterruptedException ie) {
-                System.out.println("Failed!, Interrupted");
-            }
-        }
-        if (c != null) {
-            try {
-                c.close();
-            }
-            catch (Exception e) {
-                System.out.println("Failed to close!");
-            }
-        }
+        a.disconnect();
     }
 }
