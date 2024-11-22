78c5f0b9827f7bd359bd3836f8b8a985753b13f7
priingles <kleoac1994@gmail.com>
2024-11-22 17:11:18 +0000

    mongodb dependencies added

--- src/main/java/priingles/personal/Main.java
+++ src/main/java/priingles/personal/Main.java
@@ -1,7 +1,27 @@
 package priingles.personal;
 
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("just BETTER!");
+
+        MongoClient mongoClient = new MongoClient("localhost", 27000);
+        MongoDatabase database = mongoClient.getDatabase("personal");
+        MongoCollection<Document> collection = database.getCollection("personal");
+        Document doc = new Document("name", "priingles")
+                .append("class", "software engineering")
+                .append("year", "2024")
+                .append("result", new Document("CW", 90).append("EX", 85));
+        collection.insertOne(doc);
+
+        Document myDoc = collection.find().first();
+        System.out.println(myDoc.toJson());
     }
 }
